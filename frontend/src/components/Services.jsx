2cf662ffd7f446d0745b2e2e7292d16e759d20df
snmsndhu <107799845+snmsndhu@users.noreply.github.com>
2023-10-04 13:25:35 -0400

    service section

--- frontend/src/components/Services.jsx
+++ frontend/src/components/Services.jsx
@@ -1,3 +1,6 @@
+import { BsSheildFillCheck } from "react-icons/bs";
+import { BiSearchAlt } from "react-icons/bi";
+import { RiHeart2Fill } from "react-icons/ri";
 const Service = () => {
   return <h1>Services</h1>;
 };
